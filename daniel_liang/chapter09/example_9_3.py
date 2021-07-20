957cac58d36dfccceefb1b2d1d326364751b1b75
Pankajjindal <you@example.com>
2021-07-20 06:42:22 -0700

    change

--- daniel_liang/chapter09/example_9_3.py
+++ daniel_liang/chapter09/example_9_3.py
@@ -20,6 +20,21 @@ class widgetDemo:
 
         frame2 = Frame(window)
         frame2.pack()
+        lab1=Label(frame2, text = " Enter your name:");
+        self.name = StringVar()
+        ent=Entry(frame2, textvariable=self.name)
+        msg=Message(frame2, text = " this is a widget window")
+        radb=Button(frame2, text="Get name", command=self.processRadio)
+        lab1.grid(row = 1, column = 1)
+        ent.grid(row = 1 , column = 2)
+        msg.grid(row = 1 , column = 3)
+        radb.grid(row = 1, column = 4)
+
+        text=Text(window)
+        text.pack()
+        text.insert(END,"this is the window")
+        text.insert(END,"this showsn many windows")
+
 
         window.mainloop()
 
@@ -30,5 +45,7 @@ class widgetDemo:
     def processRadioButton(self):
         print("processRadioButton");
 
+    def processRadio(self):
+        print("processRadio your name is " + self.name.get());
 
 widgetDemo()
